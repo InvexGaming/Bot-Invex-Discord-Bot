e04a8f8d7cb18edd55ae93f4d0002b045c2961c3
Mohammad Ghasembeigi <me@mohammadg.com>
2017-09-29 21:37:18 +1000

    Optimize config.py to only read config.ini once and return same global

--- config.py
+++ config.py
@@ -2,9 +2,16 @@ import configparser
 
 ''' Config import file which can be included to access config values '''
 
+config = None
+
 # Parse config
 def GetConfig():
-    config = configparser.ConfigParser()
-    config.sections()
-    config.read('config.ini')
+    global config
+    
+    # Only read once
+    if config is None:
+        config = configparser.ConfigParser()
+        config.sections()
+        config.read('config.ini')
+    
     return config
